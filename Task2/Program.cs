46492b53aff2e6b4bf3ff95fce26f8ff36dda0ea
Mohammad-Al-Sharef <www.malsharef57@gmail.com>
2024-10-07 14:29:22 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //Console.WriteLine(" please Enter  Number 1 ");
+            //int number_1= Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(" please Enter  Number 2 ");
+            //int number_2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (number_1 > number_2)
+            //{
+
+            //    Console.WriteLine(" The largest number is : " + number_1);
+
+            //}
+            //else {
+            //    Console.WriteLine(" The largest number is : " + number_2);
+
+            //}
+
+
+            //Q2
+            //Console.Write("Please Enter  number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("The sign  positive");
+            //}
+            //else if (number < 0)
+            //{
+            //    Console.WriteLine("The sign negative ");
+            //}
+
+            //Q3
+            //int[] Number = { 0,-1,4 };
+            //Array.Sort(Number);
+
+            //foreach (int i in Number)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Q4
+
+            //int[] numbers = { -5, -2, -6, 0, -1 };
+
+            //int max = numbers[0];
+            ////by using foreach
+            //foreach (int num in numbers)
+            //{
+            //    if (num > max)
+            //    {
+            //        max = num;
+            //    }
+            //}
+
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine("The maximum number is: " + max);
+
+            //Q5
+            //const double miles = 0.621;
+
+            //Console.Write("Please Enter  kilometers per hour : ");
+            //double kilometers = Convert.ToDouble(Console.ReadLine());
+
+            //double miles_Per_Hour = kilometers * miles;
+
+            //Console.WriteLine("miles Per Hour : " + miles_Per_Hour);
+
+            //Q6
+            //Console.Write("please Enter hours : ");
+            //int hours = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Please Enter minutes : ");
+            //int minutes = Convert.ToInt32(Console.ReadLine());
+
+            //int Full_time = (hours * 60) + minutes;
+
+            //Console.WriteLine(" Full time : " + Full_time + " minutes");
+
+
+            //Q7
+            //Console.Write("Please Enter minutes: ");
+            //int total_Minutes = Convert.ToInt32(Console.ReadLine());
+
+            //int hours = total_Minutes / 60;
+            //int minutes = total_Minutes % 60;
+
+            //Console.WriteLine(" hours: " + hours + " minutes: "+ minutes);
+
+
+
+            //Q8
+
+            //string[] name = {"mohammad","ali","non","morad"};
+            //int lengthname = 3;
+
+            //foreach (string n in name)
+            //{
+
+            //    if (n.Length >= lengthname)
+            //    {
+            //        Console.WriteLine(n.Substring(0, lengthname));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(n);
+            //    }
+            //}
+
+
+            //Q9 No I didn't know
+
+        }
+    }
+}
